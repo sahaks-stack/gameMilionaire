9c539ce5384fcf9d61f81b4b484666c995895de9
sahaks-stack <sahak.s@tumo.org>
2026-07-25 17:46:20 +0400

    add animation fail

--- js/script.js
+++ js/script.js
@@ -3,10 +3,58 @@
 let mainGame = document.querySelector('.game-block'),
 gameWrapper = document.querySelector('.game-wrap'),
 startBtn = document.querySelector('.start-btn'),
-enqBtn = document.querySelector('.end-btn'),
+endBtn = document.querySelector('.end-btn'),
 btnAnswers = document.querySelector('.answer'),
 blockQuestion = document.querySelector('.question'),
 helpBtns = document.querySelector('.hints-help'),
-winBlock = document.querySelector('.wins-block')
+winBlock = document.querySelector('.wins-block'),
+helpFtfty = document.querySelector('.fifty-fifty'),
+helpHall = document.querySelector('.hall-help'),
+helpFriend = document.querySelector('.call-friend'),
+helpAI = document.querySelector('.ai-help'),
+game = document.querySelector('.game')
+
+let changeQuestion = document.getElementById('hintBox')
+let extraQuestion = document.getElementById('extra')
+let flagExtra = true
+let endB = document.getElementById('end')
+
+const popup = document.getElementById('rulesPopup')
+const showBtn = document.getElementById('showRules')
+
+let aiExplainBlock = document.getElementById('aiExplainBlock');
+let aiExplainText = document.getElementById('aiExplainText');
+let aiExplainClose = document.getElementById('aiExplainClose');
+
+const OPENAI_KEY = ''
+const OPENAI_MODEL = ''
+
+showBtn.addEventListener('click' , function(){
+    popup.classList.add('show')
+})
+
+popup.addEventListener('click' , function(){
+    popup.classList.remove('show')
+})
 
 
+const generalMusic = new Audio ('./music/end-sound.mp3')
+const questionSong = new Audio ('./music/questions-sound.mp3')
+let count = 0
+
+let fixed1 = new Audio ('./music/8,000-question.mp3')
+let incorrecSoundFlag = false
+
+generalMusic.loop = true
+
+window.addEventListener('click' , () => {
+    generalMusic.play()
+}, {onse: true})
+
+endBtn.addEventListener('click' , () => {
+    setTimeout (() => {
+        game.style.backgroundImage = ""
+    },)
+    questionSong.pause()
+    mainGame.classList.remove('animate__backInUp')
+})
